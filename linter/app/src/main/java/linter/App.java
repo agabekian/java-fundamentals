c7c4c03b2000c4a8b3ffd8754b8a4a82eb0b1648
Lolunts <armasconi@gmail.com>
2022-10-26 18:57:44 -0700

    clunky-linter1

---
+++ linter/app/src/main/java/linter/App.java
@@ -0,0 +1,44 @@
+package linter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+//        Path linterPath = Paths.get("gates.js");
+//        System.getProperty("user.dir"); //from Ben!
+        readBufferTryWithResources("gates.js");
+    }
+
+    // Read with BufferedReader try with resources
+    public static void readBufferTryWithResources(String filePath) {
+        Path path = Paths.get(filePath);
+        int counter = 1;
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line = reader.readLine();
+
+            line = reader.readLine();
+            while (line != null) {
+                boolean goAhead = true;
+                counter += 1;
+                if (line.isEmpty() || line.endsWith("{") || line.endsWith("}") || line.contains("if") || line.contains("else")) {
+                    goAhead = false;
+                    System.out.println("do not touch me");
+                }
+                else if (goAhead  && !line.endsWith(";")) {
+                    System.out.println("Line " + counter + " missing semicolon.");
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
+
