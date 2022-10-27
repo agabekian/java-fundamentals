f33dcf154320d455fc5d9e9adcc758a72a073632
Lolunts <armasconi@gmail.com>
2022-10-26 19:15:05 -0700

    clunky-linter1.1

--- linter/app/src/main/java/linter/App.java
+++ linter/app/src/main/java/linter/App.java
@@ -10,6 +10,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    public String getGreeting(){
+        return "That was something...";
+    }
     public static void main(String[] args) {
 //        Path linterPath = Paths.get("gates.js");
 //        System.getProperty("user.dir"); //from Ben!
@@ -19,17 +22,18 @@ public class App {
     // Read with BufferedReader try with resources
     public static void readBufferTryWithResources(String filePath) {
         Path path = Paths.get(filePath);
-        int counter = 1;
+        int counter = 0;
         try (BufferedReader reader = Files.newBufferedReader(path)) {
             String line = reader.readLine();
 
-            line = reader.readLine();
+//            line = reader.readLine();
             while (line != null) {
+//                System.out.println(line);
                 boolean goAhead = true;
                 counter += 1;
                 if (line.isEmpty() || line.endsWith("{") || line.endsWith("}") || line.contains("if") || line.contains("else")) {
                     goAhead = false;
-                    System.out.println("do not touch me");
+//                    System.out.println("do not touch me");
                 }
                 else if (goAhead  && !line.endsWith(";")) {
                     System.out.println("Line " + counter + " missing semicolon.");
--- linter/gates.js
+++ linter/gates.js
@@ -1,4 +1,4 @@
-function and(a,b) {
+function and(a,b)
   if (a === 1 && b === 1) {
     return 1
   } else {
